1659bdd7e3e5edd84832debce8319d9e9585270d
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-11-02 14:22:29 -0400

    ISYS2160

--- Swift iOS App Development/Weather/AppDelegate.swift
+++ Swift iOS App Development/Weather/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSPlacesClient.provideAPIKey("AIzaSyAsx1fscTan2Ptak52dHAgdamQ4OiPjxnY")
+        GMSPlacesClient.provideAPIKey("")//API KEY REMOVED
         return true
     }
 
